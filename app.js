476d0a01f23f9a39f4fb79528ea9a79f614d2b1f
Ibrahim Joseph <josephibrahi@gmail.com>
2019-06-17 23:44:59 +0100

    Get query to return user course if found, else return error.

--- app.js
+++ app.js
@@ -3,17 +3,29 @@
 const express = require("express");
 const app = express();
 
+// model course in array
+const courses = [
+  { id: 1, courae: "course1" },
+  { id: 2, courae: "course2" },
+  { id: 3, courae: "course3" }
+];
 app.get("/", function(req, res) {
   res.send("Hello world!!");
 });
 
 app.get("/api/courses", function(req, res) {
-  res.send([1, 2, 3, 4]);
+  res.send(courses);
 });
 
 // get book by id
 app.get("/api/courses/:id", (req, res) => {
-  res.send(req.params.id);
+  //  loop over course to find the course you are looking for
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+
+  // send message if not found
+  if (!course) res.status(404).send("The course withthe given ID is not found");
+  // return found course
+  return res.send(course);
 });
 
 // date and month in the request params
@@ -22,9 +34,9 @@ app.get("/api/courses/:id", (req, res) => {
 // });
 
 // getting query paramsw fro request
-app.get("/api/post/:year/:month", (req, res) => {
-  res.send(req.query);
-});
+// app.get("/api/post/:year/:month", (req, res) => {
+//   res.send(req.query);
+// });
 // define an environmental value
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
