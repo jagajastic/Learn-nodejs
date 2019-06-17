bfeb4a68c5da48f11c987758bbf29a8a413a0c63
Ibrahim Joseph <josephibrahi@gmail.com>
2019-06-17 23:32:11 +0100

    Learn how to get data from req.params and req.query object

--- app.js
+++ app.js
@@ -11,4 +11,20 @@ app.get("/api/courses", function(req, res) {
   res.send([1, 2, 3, 4]);
 });
 
-app.listen(3000, () => console.log("Listening on port 3000..."));
+// get book by id
+app.get("/api/courses/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+// date and month in the request params
+// app.get("/api/post/:year/:month", (req, res) => {
+//   res.send(req.params);
+// });
+
+// getting query paramsw fro request
+app.get("/api/post/:year/:month", (req, res) => {
+  res.send(req.query);
+});
+// define an environmental value
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
