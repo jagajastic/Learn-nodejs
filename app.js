7cc673d166ff5e2711f914b8cadcbf8ea4d999c0
Ibrahim Joseph <josephibrahi@gmail.com>
2019-06-22 15:28:57 +0100

    Implement edit(PUT) hhtp method

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ app.use(express.json());
 
 // model course in array
 const courses = [
-  { id: 1, course: "course1" },
-  { id: 2, course: "course2" },
-  { id: 3, course: "course3" }
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" }
 ];
 app.get("/", function(req, res) {
   res.send("Hello world!!");
@@ -35,9 +35,7 @@ app.post("/api/courses", (req, res) => {
   // if error send error to client
   // simplify the error message send to the client to easy of rendering
   if (result.error)
-    return res.status(400).send(result.error.details[0].message );
-
-  console.log(result);
+    return res.status(400).send(result.error.details[0].message);
   const course = {
     id: courses.length + 1,
     name: req.body.name
@@ -56,6 +54,30 @@ app.get("/api/courses/:id", (req, res) => {
   return res.send(course);
 });
 
+// implementating update (PUT method)
+app.put("/api/courses/:id", (req, res) => {
+  // first of all look up the course in db
+  // if not exist return 4040(not found)
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send(`Course with ID ${courseId} not found`);
+
+  // validate the course
+  // if invalid, return 404 - bad request
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+  const result = Joi.validate(req.body, schema);
+  if (result.error)
+    return res.status(400).send(result.error.details[0].message);
+  // push the value from the user request body to update the course
+  // update course
+  // return the course to the client
+  course.name = req.body.name;
+  res.status(200).send(course);
+});
 // date and month in the request params
 // app.get("/api/post/:year/:month", (req, res) => {
 //   res.send(req.params);
@@ -66,5 +88,5 @@ app.get("/api/courses/:id", (req, res) => {
 //   res.send(req.query);
 // });
 // define an environmental value
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
